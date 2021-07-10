4a8f22b98cc445e9292ce40a79b6ed1093aef3bd
235illino <235illino@gmail.com>
2021-07-10 14:53:20 +0300

    ДЗ No1

---
+++ src/main/java/homework_30_06/GenericExample.java
@@ -0,0 +1,22 @@
+package homework_30_06;
+
+public class GenericExample<K, V> {
+    private K key;
+    private V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
